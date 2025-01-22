ce2a8c76f7c47ec7e0f7fb1fff205c5388e01d28
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-22 13:13:52 +0700

    update

--- Digiverse/run.sh
+++ Digiverse/run.sh
@@ -30,16 +30,13 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "howManyPointsDoYouWantWhenPlayGame": 10000,
+  "referralCode": "1b2de355f987134afc79098d3e79aae1",
+  "delayEachAccount": [1, 81],
+  "doTasks": true
 }
 EOL
 }
@@ -55,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    DIgiverse BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +112,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
